7f2845529926f1d4e950f2f26309195a30ab3b96
Leandro Rosales <leanrosales43@gmail.com>
2026-05-06 14:35:25 -0400

    agregue un comentario en la linea 13 de el index a la verga

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const viajesRutas = require("./src/routes/viajeRutas");
+
+const app = express();
+const PUERTO = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/viajes", viajesRutas);
+//pinche pruebita a la verga
+
+app.listen(PUERTO, () => {
+  console.log(`Servidor de Peke Trak corriendo al 100% en el puerto ${PUERTO}`);
+});
---
+++ backend/src/data/viajeData.js
@@ -0,0 +1,18 @@
+const viajeActual = {
+  ruta: "Ruta Centro - Zona Norte",
+  conductor: "Freddy Vargas",
+  bus: "BUS-001",
+  estudiante: {
+    nombre: "Matías Aranibar",
+    colegio: "Colegio La Salle",
+    destino: "Av: Villaroel Esq Beni",
+  },
+  estado_viaje: {
+    distancia_km: 1.0,
+    tiempo_restante_min: 5,
+    hora_estimada: "5:45 PM",
+  },
+};
+
+// Exportamos los datos para que otros archivos los puedan usar
+module.exports = viajeActual;
